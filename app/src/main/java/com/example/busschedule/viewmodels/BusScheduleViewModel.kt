82dd206c2e7a4a5773170fdac763a24699e59a13
Imron <imron.gamidli@lt.hs-fulda.de>
2022-06-26 01:05:56 +0200

    feat: use the adapter in StopScheduleFragment and use flow for changes

--- app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
@@ -3,8 +3,11 @@ package com.example.busschedule.viewmodels
 import androidx.lifecycle.ViewModel
 import com.example.busschedule.database.schedule.Schedule
 import com.example.busschedule.database.schedule.ScheduleDao
+import kotlinx.coroutines.flow.Flow
+
 
 class BusScheduleViewModel(private val scheduleDao: ScheduleDao) : ViewModel() {
-  fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
-  fun scheduleForStopName(stopName: String): List<Schedule> = scheduleDao.getByStopName(stopName)
+  fun fullSchedule(): Flow<List<Schedule>> = scheduleDao.getAll()
+  fun scheduleForStopName(stopName: String): Flow<List<Schedule>> =
+    scheduleDao.getByStopName(stopName)
 }
