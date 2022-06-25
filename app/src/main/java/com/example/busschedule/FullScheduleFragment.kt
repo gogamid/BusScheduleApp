91952f7d95b0af16824713c53e7ce60d15dba438
Imron <imron.gamidli@lt.hs-fulda.de>
2022-06-26 00:42:27 +0200

    feat: use the adapter in FullScheduleFragment

--- app/src/main/java/com/example/busschedule/FullScheduleFragment.kt
+++ app/src/main/java/com/example/busschedule/FullScheduleFragment.kt
@@ -20,35 +20,59 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.busschedule.databinding.FullScheduleFragmentBinding
+import com.example.busschedule.viewmodels.BusScheduleViewModel
+import com.example.busschedule.viewmodels.BusScheduleViewModelFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-class FullScheduleFragment: Fragment() {
+class FullScheduleFragment : Fragment() {
 
-    private var _binding: FullScheduleFragmentBinding? = null
+  private var _binding: FullScheduleFragmentBinding? = null
 
-    private val binding get() = _binding!!
+  private val binding get() = _binding!!
 
-    private lateinit var recyclerView: RecyclerView
+  private lateinit var recyclerView: RecyclerView
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FullScheduleFragmentBinding.inflate(inflater, container, false)
-        return binding.root
+  private val viewModel: BusScheduleViewModel by activityViewModels {
+    BusScheduleViewModelFactory(
+      (activity?.application as BusScheduleApplication).database.scheduleDao()
+    )
+  }
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    _binding = FullScheduleFragmentBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    recyclerView = binding.recyclerView
+    recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+    val busStopAdapter = BusStopAdapter {
+      val action = FullScheduleFragmentDirections.actionFullScheduleFragmentToStopScheduleFragment(
+        stopName = it.stopName
+      )
+      view.findNavController().navigate(action)
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        recyclerView = binding.recyclerView
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+    recyclerView.adapter = busStopAdapter
+    GlobalScope.launch(Dispatchers.IO) {
+      busStopAdapter.submitList(viewModel.fullSchedule())
     }
+  }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
 }
